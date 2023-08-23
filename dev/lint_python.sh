bc473fc041a2e4aa3cc963cca0f3dbe1b557a319
Yu Ishikawa <yu-iskw@users.noreply.github.com>
2023-08-23 15:09:35 +0900

    Use lightdash-client-python==0.753.0 (#20)

--- dev/lint_python.sh
+++ dev/lint_python.sh
@@ -24,4 +24,4 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 PROJECT_DIR="$(dirname "$SCRIPT_DIR")"
 
 pylint -v "${PROJECT_DIR}/lightdash_ops"
-mypy "${PROJECT_DIR}/lightdash_ops"
+mypy --show-error-context "${PROJECT_DIR}/lightdash_ops"
